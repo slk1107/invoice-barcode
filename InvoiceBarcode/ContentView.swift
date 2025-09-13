e7a657fbc9c8cd821267c597c3a04a520dfad064
slk11075 <samuel1107>
2025-09-13 21:16:42 +0800

    Initial Commit

---
+++ InvoiceBarcode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InvoiceBarcode
+//
+//  Created by Kris Lin on 2025/9/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InvoiceBarcode/InvoiceBarcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvoiceBarcodeApp.swift
+//  InvoiceBarcode
+//
+//  Created by Kris Lin on 2025/9/13.
+//
+
+import SwiftUI
+
+@main
+struct InvoiceBarcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
